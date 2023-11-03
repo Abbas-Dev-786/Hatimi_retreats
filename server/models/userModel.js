cd48c8bd366aa86ccc74e311f29dfef302089438
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-11-03 21:47:56 +0530

    added admin auth & data models

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 const mongoosePaginate = require("mongoose-paginate-v2");
-const AppError = require("../utils/AppError");
-const Property = require("./propertyModel");
 
 const ROLES = ["admin", "user"];
 
