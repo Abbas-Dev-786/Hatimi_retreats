c5d1fde50587b13f2aee5250b953684a236c861e
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-11-22 22:16:01 +0530

    login implemented

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,8 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import "bootstrap/scss/bootstrap.scss";
 //eslint-disable-next-line
 import * as bootstrap from "bootstrap";
-
-import Aos from "aos";
-import "aos/dist/aos.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Login from "./pages/Login";
@@ -15,29 +11,28 @@ import Courts from "./pages/Courts";
 import Request from "./pages/Request";
 
 import Notification from "./components/common/Notification";
+import PageNotFound from "./pages/PageNotFound";
+import ProtectedRoute from "./components/common/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 0 } },
 });
 
 const App = () => {
-  useEffect(() => {
-    Aos.init();
-    // Aos.refresh();
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <Notification />
       <div className="main-wrapper">
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route index path="dashboard" element={<Dashboard />} />
+            <Route path="courts" element={<Courts />} />
+            <Route path="request" element={<Request />} />
+          </Route>
 
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/courts" element={<Courts />} />
-          <Route path="/request" element={<Request />} />
-  
+
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
     </QueryClientProvider>
--- admin/src/components/login/Form.jsx
+++ admin/src/components/login/Form.jsx
@@ -1,13 +1,46 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { ArrowRightCircle, Eye, EyeOff } from "react-feather";
+import { toast } from "react-toastify";
+import { LoginUser } from "../../state/api";
+import { login } from "../../state/slices/userSlice";
 
 const Form = () => {
   const [isPasswordHidden, setPasswordHidden] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+  const { mutate, isLoading } = useMutation({
+    mutationFn: LoginUser,
+    onError: (err) => {
+      toast.error(err.message);
+    },
+    onSuccess: (data) => {
+      dispatch(login(data));
+
+      navigate("/dashboard");
+      toast.success("Login Successfull");
+    },
+  });
 
   const handleLoginFormSubmission = (e) => {
     e.preventDefault();
+
+    if (!email || !password) {
+      toast.error("All Fields are Mandatory.");
+      return;
+    }
+
+    if (password.length < 8) {
+      toast.error("Password should be 8 characters long.");
+      return;
+    }
+
+    mutate({ email, password });
   };
 
   return (
@@ -16,14 +49,12 @@ const Form = () => {
         <div className="row no-margin vph-100 d-flex align-items-center justify-content-center">
           <div className="col-sm-10 col-md-10 col-lg-10 mx-auto">
             <header className="text-center">
-              <a href="user-dashboard.html">
-                <img
-                  src="/img/logo-black.png"
-                  className="img-fluid"
-                  alt="Logo"
-                  width="110px"
-                />
-              </a>
+              <img
+                src="/img/logo-black.png"
+                className="img-fluid"
+                alt="Logo"
+                width="110px"
+              />
             </header>
             <div className="shadow-card">
               <h2>Welcome Back</h2>
@@ -67,26 +98,27 @@ const Form = () => {
                         />
                       </div>
                     </div>
-                    <div className="form-group d-sm-flex align-items-center justify-content-between">
-                      <div className="form-check form-switch d-flex align-items-center justify-content-start">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          id="user-pass"
-                        />
-                        <label className="form-check-label" htmlFor="user-pass">
-                          Remember Password
-                        </label>
-                      </div>
-                      {/* <span>
-                        <a href="forgot-password.html" className="forgot-pass">
-                          Forgot Password
-                        </a>
-                      </span> */}
-                    </div>
+                    {/* <div className="form-group d-sm-flex align-items-center justify-content-between">
+                        <div className="form-check form-switch d-flex align-items-center justify-content-start">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="user-pass"
+                          />
+                          <label className="form-check-label" htmlFor="user-pass">
+                            Remember Password
+                          </label>
+                        </div>
+                        <span>
+                          <a href="forgot-password.html" className="forgot-pass">
+                            Forgot Password
+                          </a>
+                        </span>
+                      </div> */}
                     <button
                       className="btn btn-secondary register-btn d-inline-flex justify-content-center align-items-center w-100 btn-block"
                       type="submit"
+                      disabled={isLoading}
                     >
                       Sign In
                       <i className="ms-2">
