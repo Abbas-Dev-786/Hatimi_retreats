8d06aa6d3efcfd0e2218bc1006118c2dfa7f844a
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-11-28 09:37:41 +0530

    updated cors policy

--- server/app.js
+++ server/app.js
@@ -42,8 +42,8 @@ const corsOptions = {
   },
   credentials: true,
 };
-app.use(cors());
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 // Set security HTTP headers
 app.use(helmet());
